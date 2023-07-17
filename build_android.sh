e81310e4d6793aea0cc8d28284231ee45c9bffdf
Andrea Raffin <andrea.raffin.88@gmail.com>
2023-07-17 21:10:53 +0200

    enhance script

--- build_android.sh
+++ build_android.sh
@@ -11,41 +11,66 @@ echo ""
 
 echo "Preparing working files and directories"
 
-rm -rf build_android
-rm -rf CMakeCache.txt
-mkdir -p build_android
-cd build_android
+output_folder=build_libs_android-$ANDROID_NATIVE_API_LEVEL 
+
+mkdir -p ${output_folder} # FINAL OUTPUTS ARE COPIED HERE
+
 
 declare -a AndroidArchitectures=("x86" "x86_64" "arm" "arm64")
+# declare -a AndroidArchitectures=("arm")
 declare -a iOSArchitectures=("x86_64" "arm64" "arm64e")
 
 LibraryName="OpenCVMobile"
 Android_NDK_Host_Name="linux"
 Android_NDK_Host_Tag="linux-x86_64"
-Android_Minimum_Api_Version="33"
+Android_Minimum_Api_Version=$ANDROID_NATIVE_API_LEVEL
 iOS_SDK_Version="12.2"
 iOS_SDK_Min_Version="8.0"
+
+## Dynamic or Static library output
+## ON = *.so ; OFF = *.a
+SET_SHARED=ON
 
 echo ""
 echo "=== BUILD TARGET (Android) ==="
 echo ""
 echo "PWD: ${PWD}"
 
-# cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local 
+for i in "${AndroidArchitectures[@]}"
+    do
+        echo "Build for $i:"
 
-# cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang++ -DANDROID_ABI=arm64-v8a -D CMAKE_BUILD_TYPE=Release -D ANDROID_NATIVE_API_LEVEL=33 -D WITH_CUDA=OFF -D WITH_MATLAB=OFF -D BUILD_ANDROID_EXAMPLES=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D ANDROID_STL=c++_shared -D BUILD_SHARED_LIBS=ON -D BUILD_opencv_objdetect=OFF -D BUILD_opencv_video=OFF -D BUILD_opencv_videoio=OFF -D BUILD_opencv_features2d=OFF -D BUILD_opencv_flann=OFF -D BUILD_opencv_highgui=ON -D BUILD_opencv_ml=ON -D BUILD_opencv_photo=OFF -D BUILD_opencv_python=OFF -D BUILD_opencv_shape=OFF -D BUILD_opencv_stitching=OFF -D BUILD_opencv_superres=OFF -D BUILD_opencv_ts=OFF -D BUILD_opencv_videostab=OFF -DBUILD_ANDROID_PROJECTS=OFF ..
+        ABI_Folder_Name=$i
 
-# cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang++ \
-#     -DBUILD_LIST=core,imgproc -DCMAKE_INSTALL_PREFIX=install \
-#     -DCMAKE_BUILD_TYPE=Release \
-#     `cat ../../opencv-mobile/opencv4_cmake_options.txt` \
-#     -DBUILD_opencv_world=OFF ..
+        if [ $i == "arm" ]
+        then
+            ABI_Folder_Name="armeabi-v7a"
+        elif [ $i == "arm64" ]
+        then
+            ABI_Folder_Name="arm64-v8a"
+        fi
 
-# cat ../../opencv-mobile/opencv4_cmake_options.txt
+        # Temp build folder
+        rm -rf _build_android_$ABI_Folder_Name-$ANDROID_NATIVE_API_LEVEL 
+        rm -rf CMakeCache.txt
+        mkdir -p _build_android_$ABI_Folder_Name-$ANDROID_NATIVE_API_LEVEL 
+        cd _build_android_$ABI_Folder_Name-$ANDROID_NATIVE_API_LEVEL 
+        
+        cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_USE_LEGACY_TOOLCHAIN_FILE=False \
+                -DANDROID_ABI=${ABI_Folder_Name} -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-33 -DANDROID_CPP_FEATURES="no-rtti no-exceptions" \
+                -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=${SET_SHARED} `cat ../opencv4_cmake_options.txt` -DBUILD_opencv_world=OFF -DOPENCV_DISABLE_FILESYSTEM_SUPPORT=ON ..
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_USE_LEGACY_TOOLCHAIN_FILE=False \
-        -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-33 -DANDROID_CPP_FEATURES="no-rtti no-exceptions" \
-        -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release `cat ../../opencv-mobile/opencv4_cmake_options.txt` -DBUILD_opencv_world=OFF -DOPENCV_DISABLE_FILESYSTEM_SUPPORT=ON ..
+        make -j20
+
+        echo "Copying /lib/$ABI_Folder_Name to ../$output_folder"
+        cp -R lib/$ABI_Folder_Name ../$output_folder
+
+    done
+
+cd ..
+echo "** BUILD SUCCEEDED (Android) **"
+echo ""     
+
         
 
 # cmake -D CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
@@ -56,6 +81,5 @@ cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -D
 #       -D BUILD_LIST=core,imgproc \
 #       ..
 
-make -j20
 # make install
 # make clean
